e13caf278350fdb5ca132cca48d1268348e40640
Clacson <cookiellacson@gmail.com>
2017-01-16 21:38:17 +0000

    one show rendered from shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,15 @@
 class ShowsController < ApplicationController
 
+  def index
+    @shows = [
+      {title: "The Great British Bakeoff", 
+        series: 7,
+        description: "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
+        image: "placeholder.jpg",
+        programmeID: "123456"
+      }
+    ]
+    render :json => @shows
+  end
+
 end
