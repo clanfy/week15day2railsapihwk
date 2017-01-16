ae4d5071e95a777c69f37f26991bb26bfa05ca60
Clacson <cookiellacson@gmail.com>
2017-01-16 22:17:53 +0000

    successfully creates and posts new show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -10,4 +10,15 @@ class ShowsController < ApplicationController
     render :json => @show
   end
 
+  def create
+    show = Show.create(show_params)
+      render :json => show
+  end
+
+  private 
+
+  def show_params
+    params.require(:show).permit([:title, :series, :description, :image, :programmeID])
+  end
+
 end
