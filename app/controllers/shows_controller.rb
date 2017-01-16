956077d165c485d2f1c616f67dba82fe26f519a8
Clacson <cookiellacson@gmail.com>
2017-01-16 21:56:05 +0000

    successfully creates shows in seeds file

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,14 +1,7 @@
 class ShowsController < ApplicationController
 
   def index
-    @shows = [
-      {title: "The Great British Bakeoff", 
-        series: 7,
-        description: "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
-        image: "placeholder.jpg",
-        programmeID: "123456"
-      }
-    ]
+    @shows = Show.all
     render :json => @shows
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Show.delete_all
+
+Show.create(
+        {title: "UnReal", 
+        series: 2,
+        description: "Strong female leads manipulating other female leads in an unsavoury look at Reality Television and romance. Based on The American hit TV show The Bachelor",
+        image: "everlasting.jpg",
+        programmeID: "123456"})
+
+Show.create(
+        {title: "Atlanta", 
+        series: 1,
+        description: "Golden Globe winning actor and writer Donald Glover stars in this satirical dark comedy about a strruggling producer trying and usually failing to get his client's rap career and his own life on track and onto success.",
+        image: "peaches.jpg",
+        programmeID: "123123"})
